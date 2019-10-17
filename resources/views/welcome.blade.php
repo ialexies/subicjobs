415cb04d53021d350ce3a147b297362e7ac476e7
ialexies <ialexies@gmail.com>
2019-10-18 07:49:09 +0800

    subic jobs title test push pull

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Subic Jobs
                 </div>
 
                 <div class="links">
